5e4bb93bf11b688f767146e21524cbe3cb7b4edc
puthrichard <puthrichard@gmail.com>
2017-08-29 22:25:33 -0400

    Necessary files to commit for initial files

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Richard Puth on 2017-08-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
